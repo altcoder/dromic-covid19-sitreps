c687956e09f4644da845d5b8031c0257126b2698
James Faeldon <111378+altcoder@users.noreply.github.com>
2020-04-11 07:55:56 +0800

    Added script to run all tasks

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+airflow test etl_dromic_covid19_sitreps_fas execute_notebook 2020-04-04
+airflow test etl_dromic_covid19_sitreps_coa execute_notebook 2020-04-04
+airflow test etl_dromic_covid19_sitreps_fas upload_to_gsheet 2020-04-04
+airflow test etl_dromic_covid19_sitreps_coa upload_to_gsheet 2020-04-04
+airflow test etl_dromic_covid19_sitreps_fas upload_to_s3 2020-04-04
+airflow test etl_dromic_covid19_sitreps_coa upload_to_s3 2020-04-04
+airflow test etl_dromic_covid19_sitreps_fas upload_to_snowflake 2020-04-04
+airflow test etl_dromic_covid19_sitreps_coa upload_to_snowflake 2020-04-04
