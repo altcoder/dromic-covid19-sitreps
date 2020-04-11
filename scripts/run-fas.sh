488e2ebb3eca1bca1a2a901dbd3d92b3f446084f
James Faeldon <111378+altcoder@users.noreply.github.com>
2020-04-11 23:58:56 +0800

    Updated for sitrep 44 and 45

--- scripts/run-fas.sh
+++ scripts/run-fas.sh
@@ -1,8 +1,4 @@
 airflow test etl_dromic_covid19_sitreps_fas execute_notebook 2020-04-04
-airflow test etl_dromic_covid19_sitreps_coa execute_notebook 2020-04-04
 airflow test etl_dromic_covid19_sitreps_fas upload_to_gsheet 2020-04-04
-airflow test etl_dromic_covid19_sitreps_coa upload_to_gsheet 2020-04-04
 airflow test etl_dromic_covid19_sitreps_fas upload_to_s3 2020-04-04
-airflow test etl_dromic_covid19_sitreps_coa upload_to_s3 2020-04-04
 airflow test etl_dromic_covid19_sitreps_fas upload_to_snowflake 2020-04-04
-airflow test etl_dromic_covid19_sitreps_coa upload_to_snowflake 2020-04-04
