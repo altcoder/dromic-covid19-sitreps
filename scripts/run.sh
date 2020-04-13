acd24035f2027907f4caf9410e22fd98e5c29f2c
James Faeldon <111378+altcoder@users.noreply.github.com>
2020-04-13 10:12:09 +0800

    Updated for SitRep 47

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
+airflow test webpage_poll_trigger check_webpage_dromic_covid19_sitreps 2020-04-04
 airflow test etl_dromic_covid19_sitreps_fas execute_notebook 2020-04-04
 airflow test etl_dromic_covid19_sitreps_coa execute_notebook 2020-04-04
 airflow test etl_dromic_covid19_sitreps_fas upload_to_gsheet 2020-04-04
