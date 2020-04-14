dc48076e4679c4fe4b9b596129de81a3afb798c3
James Faeldon <111378+altcoder@users.noreply.github.com>
2020-04-14 08:19:02 +0800

    Updated for sitrep 49

---
+++ scripts/run-coa-exec.sh
@@ -0,0 +1,1 @@
+airflow test etl_dromic_covid19_sitreps_coa execute_notebook 2020-04-04
---
+++ scripts/run-coa-upload.sh
@@ -0,0 +1,3 @@
+airflow test etl_dromic_covid19_sitreps_coa upload_to_gsheet 2020-04-04
+airflow test etl_dromic_covid19_sitreps_coa upload_to_s3 2020-04-04
+airflow test etl_dromic_covid19_sitreps_coa upload_to_snowflake 2020-04-04
